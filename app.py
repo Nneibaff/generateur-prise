caa71eafa4fc5e761dcef91ea414dfb48663e91f
fabien <fabien.peyraud@orange.com>
2025-03-17 14:16:39 +0100

    first commit

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 
 # initialize the Flask app
-from flask import Flask, request, jsonify
+from flask import Flask
 app = Flask(__name__)
 
+# define a route for the default URL
+@app.route('/')
+def hello():
+    return "Hello World!"
 
 
 if __name__ == '__main__':
